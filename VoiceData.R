84e580cd61a1b08d9d22729166fad942c163ff2f
eystdahl <93379583+eystdahl@users.noreply.github.com>
2023-03-19 18:23:30 +0100

    Add files via upload

--- VoiceData.R
+++ VoiceData.R
@@ -16,7 +16,6 @@ chisq.test(VoiceMarkersGreek)
 
 #Get relative frequencies
 VoiceMarkersGreekProportion <- prop.table(VoiceMarkersGreek, 2)*100
-
 VoiceMarkersGreekProportion
 
 #Make plot
@@ -54,9 +53,6 @@ legend("bottomright", lty = c(1,2), col = c("black", "darkgrey"), pch = c(2, 0),
 
 colnames(VoiceMarkersGreekReduced) <- c("Homer", "Herodotus", "Plato", "Polybius", "Plutarch")
 rownames(VoiceMarkersGreek) <- c("Passive aorist", "Middle aorist")
-
-
-
 
 #Get expected values
 
@@ -112,6 +108,93 @@ fisher.test(PassiveAoristPlutharch, alternative = "less")
 MiddleAoristPlutarch <- matrix(c(248, 9188, 199, 6805), ncol = 2, byrow = TRUE)
 fisher.test(MiddleAoristPlutarch, alternative = "greater")
 
+#Absolute frequencies BH binyanim
+BHbinyanim <- matrix(c(11499, 446, 988, 6, 1753, 113, 169,
+                       13932, 1414, 2194, 218, 2671, 128, 150,
+                       10323, 1116, 1569, 80, 2264, 184, 314), ncol = 7, byrow = TRUE)
+colnames(BHbinyanim) <- c("Qal", "Niphal", "Piel", "Pual", "Hiphil", "Hophal", "Hithpael")
+rownames(BHbinyanim) <- c("Consecutive imperfect", "Perfect","Imperfect")
+
+BHbinyanim
+
+chisq.test(BHbinyanim)
+assocstats(BHbinyanim)
+
+BHbinyanimProportion <- prop.table(BHbinyanim, 2)*100
+BHbinyanimProportion
+
+barplot(BHbinyanimProportion, ylim = range(0:100), sub = "Figure 3: Distribution of tense/aspect categories over binyanim in BH", ylab = "Relative frequency (%)", col = c("black", "grey60", "grey40"), beside = TRUE)
+legend("topright", fill = c("black", "grey60", "grey40"), legend = c("Consecutive imperfect", "Perfect", "Imperfect"))
+
+#Get expected values
+
+chisq.test(BHbinyanim)$expected
+
+#Perform Fisher test
+
+ConsQal <- matrix(c(11499, 3475, 24255, 12302), ncol = 2, byrow = TRUE)
+fisher.test(ConsQal, alternative = "greater")
+
+PerfQal <- matrix(c(13932, 6775, 21822, 9002), ncol = 2, byrow = TRUE)
+fisher.test(PerfQal, alternative = "less")
+
+IpfQal <- matrix(c(10323, 5527, 25431, 10250), ncol = 2, byrow = TRUE)
+fisher.test(IpfQal, alternative = "less")
+
+ConsNiphal <- matrix(c(446, 14528, 2530, 34027), ncol = 2, byrow = TRUE)
+fisher.test(ConsNiphal, alternative = "less")
+
+PerfNiphal <- matrix(c(1414, 19293, 1562, 29262), ncol = 2, byrow = TRUE)
+fisher.test(PerfNiphal, alternative = "greater")
+
+IpfNiphal <- matrix(c(1116, 14734, 1860, 33821), ncol = 2, byrow = TRUE)
+fisher.test(IpfNiphal, alternative = "greater")
+
+
+ConsPiel <- matrix(c(988, 13986, 3763, 32821), ncol = 2, byrow = TRUE)
+fisher.test(ConsPiel, alternative = "less")
+
+PerfPiel <- matrix(c(2194, 18513, 2557, 28267), ncol = 2, byrow = TRUE)
+fisher.test(PerfPiel, alternative = "greater")
+
+IpfPiel <- matrix(c(1569, 14281, 3182, 32499), ncol = 2, byrow = TRUE)
+fisher.test(IpfPiel, alternative = "greater")
+
+ConsPual <- matrix(c(6, 14968, 298, 26259), ncol = 2, byrow = TRUE)
+fisher.test(ConsPual, alternative = "less")
+
+PerfPual <- matrix(c(218, 20489, 86, 30738), ncol = 2, byrow = TRUE)
+fisher.test(PerfPual, alternative = "greater")
+
+IpfPual <- matrix(c(80, 15770, 214, 35467), ncol = 2, byrow = TRUE)
+fisher.test(IpfPual, alternative = "less")
+
+ConsHiphil <- matrix(c(1753, 13221, 4935,  31622), ncol = 2, byrow = TRUE)
+fisher.test(ConsHiphil, alternative = "less")
+
+PerfHiphil <- matrix(c(2671, 18036, 4017, 26807), ncol = 2, byrow = TRUE)
+fisher.test(PerfHiphil, alternative = "less")
+
+IpfHiphil <- matrix(c(2264, 13586, 4424, 31257), ncol = 2, byrow = TRUE)
+fisher.test(IpfHiphil, alternative = "greater")
+
+ConsHophal <- matrix(c(113, 14861, 312, 36245), ncol = 2, byrow = TRUE)
+fisher.test(ConsHiphil, alternative = "less")
+
+PerfHophal <- matrix(c(128, 20579, 297, 30527), ncol = 2, byrow = TRUE)
+fisher.test(PerfHophal, alternative = "less")
+
+IpfHophal <- matrix(c(184, 15666, 241, 35440), ncol = 2, byrow = TRUE)
+fisher.test(IpfHophal, alternative = "greater")
+
+ConsHithpael <- matrix(c(169, 14805, 464, 36093), ncol = 2, byrow = TRUE)
+fisher.test(ConsHithpael, alternative = "less")
+
+PerfHithpael <- matrix(c(150, 20557, 483, 30341), ncol = 2, byrow = TRUE)
+fisher.test(PerfHithpael, alternative = "less")
+
+IpfHithpael <- matrix(c(314, 15536, 319, 35362), ncol = 2, byrow = TRUE)
+fisher.test(IpfHithpael, alternative = "greater")
 
 #Greek and Hebrew equivalents
 
@@ -169,3 +252,5 @@ fisher.test(MiddleHithpael, alternative = "greater")
 
 PassiveHithpael <- matrix(c(7, 5, 99, 75), ncol = 2, byrow = TRUE)
 fisher.test(MiddleHithpael, alternative = "less")
+
+
